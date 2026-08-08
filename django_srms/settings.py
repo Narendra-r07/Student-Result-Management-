1f5dbbf0633ce628dca97b22350c9da80a12d402
Narendra Rajput <narendrarajput7777@gmail.com>
2026-08-08 14:03:31 +0530

    Update settings.py

--- django_srms/settings.py
+++ django_srms/settings.py
@@ -133,8 +133,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 #MEDIA
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media/"
